9ecb064379075de10fcb5ecb8dcbfa6bdf2172ba
Assan <shynybaev@gmail.com>
2019-11-04 18:15:55 +0600

    customized gradle project details

---
+++ backend/src/kz/example/jupiter/application/JupiterApplication.java
@@ -0,0 +1,17 @@
+package kz.example.jupiter.application;
+
+import kz.example.jupiter.controllers.ImporterController;
+import kz.example.jupiter.registers.impl.ImporterRegistr;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import({ImporterController.class, ImporterRegistr.class})
+public class JupiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JupiterApplication.class, args);
+	}
+
+}
