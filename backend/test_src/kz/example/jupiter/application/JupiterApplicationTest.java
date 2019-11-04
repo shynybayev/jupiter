7702a232344c184924fd23688c0d0704ba70f8f5
Assan <shynybaev@gmail.com>
2019-11-04 19:50:20 +0600

    add docker compose

---
+++ backend/test_src/kz/example/jupiter/application/JupiterApplicationTest.java
@@ -0,0 +1,13 @@
+package kz.example.jupiter.application;
+
+import org.junit.Test;
+
+import static org.testng.Assert.*;
+
+public class JupiterApplicationTest {
+
+    @Test
+    public void mainTest() {
+
+    }
+}
