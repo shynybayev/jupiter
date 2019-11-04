9a7b40f10a9dd75ece33163e64515dc446dbf3e1
Assan <shynybaev@gmail.com>
2019-11-04 11:22:22 +0600

    Starting an empty Spring Boot project

---
+++ src/main/java/kz/example/jupiter/jupiter/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kz.example.jupiter.jupiter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JupiterApplication.class);
+	}
+
+}
